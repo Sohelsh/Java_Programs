1588ff9bba5e9bb9c4805d1961b46199b3867c8f
Sohelsh <sohelshaikhadam@gmail.com>
2024-01-09 23:23:57 +0530

    Practice

---
+++ 02.Check_Number_Even_Or_Odd/myclass.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class myclass{
+	
+	public static void main(String[] args)
+	{
+	
+	Scanner Sc = new Scanner(System.in);
+
+	System.out.println("Enter A Number = ");
+	int No = Sc.nextInt();
+
+	if(No % 2 == 0)
+	{
+	  System.out.println("Given Number Is Even");
+	}
+	else
+	{ 
+	  System.out.println("Given Number Is Odd");
+	}
+
+
+}
+
+
+}
