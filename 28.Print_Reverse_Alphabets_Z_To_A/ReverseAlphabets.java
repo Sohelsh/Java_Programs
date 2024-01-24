f2099db5fa95767bbee47c2d988862948ae55eb2
Sohelsh <sohelshaikhadam@gmail.com>
2024-01-24 21:22:38 +0530

    Practice

---
+++ 28.Print_Reverse_Alphabets_Z_To_A/ReverseAlphabets.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ReverseAlphabets
+{
+  public static void main(String[] args)
+  {
+     char Ch = ' ';
+
+     for(Ch = 'Z';Ch >= 'A';Ch--)
+     {
+ 	System.out.print(Ch+" ");
+     }
+
+}
+}
