fd80aaa86ca10a42e41272aedbe381f587889527
Sohelsh <sohelshaikhadam@gmail.com>
2024-01-24 21:21:47 +0530

    Practice

---
+++ 27.Print_Alphabets_A_To_Z/Alphabets.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Alphabets
+{
+  public static void main(String[] args)
+  {
+     char Ch = ' ';
+
+     for(Ch = 'A';Ch <= 'Z';Ch++)
+     {
+ 	System.out.print(Ch+" ");
+     }
+
+}
+}
