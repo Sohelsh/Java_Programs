1c18c0d44d0cfa586ddb3bc8f84f17070b66088b
Sohelsh <sohelshaikhadam@gmail.com>
2024-01-29 22:45:40 +0530

    Practice

---
+++ 32.Sum_All_natural_numbers/SumNumbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class SumNumbers
+{
+  public static void main(String[] args)
+  {
+    int No = 0;
+
+    for(int i = 1; i <= 100;i++)
+    {
+        No = No + i;
+    }
+	
+      System.out.print(No);
+  }
+}
+    
+    
+    
