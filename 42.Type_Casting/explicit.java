d4f88173f02d38fcbfb21f3c3609ea4185714e26
Sohelsh <sohelshaikhadam@gmail.com>
2024-02-03 22:35:19 +0530

    Practice

---
+++ 42.Type_Casting/explicit.java
@@ -0,0 +1,15 @@
+public class explicit
+{
+  public static void main(String args[])
+  {
+   double No1 = 144.4;
+   long No2 = (long) No1;
+   int No3 = (int)No2;
+  
+   System.out.println("Before conversion, int Value"+" = "+No1);
+   System.out.println("After conversion, long Value"+" = "+No2);
+   System.out.println("After conversion, float Value"+" = "+No3);
+  }
+}
+
+ 
