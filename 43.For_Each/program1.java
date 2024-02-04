efe30cbea3c4b6b733790f4cdad4471a0bb79006
Sohelsh <sohelshaikhadam@gmail.com>
2024-02-04 22:17:21 +0530

    Practice

---
+++ 43.For_Each/program1.java
@@ -0,0 +1,12 @@
+public class program1
+{
+  public static void main(String args[])
+  {
+    int arr[] = {12,55,47,58,45};
+
+    for(int i:arr)
+    {
+      System.out.println(i);
+     }
+  }
+}
