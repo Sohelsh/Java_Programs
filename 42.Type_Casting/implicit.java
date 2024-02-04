aabbd8f74a99ce73eef9d87eca0707c3116f5ee9
Sohelsh <sohelshaikhadam@gmail.com>
2024-02-04 22:16:57 +0530

    Practice

---
+++ 42.Type_Casting/implicit.java
@@ -0,0 +1,15 @@
+public class implicit
+{
+  public static void main(String args[])
+  {
+   int No1 = 10;
+   long No2 = No1;
+   float No3 = No2;
+  
+   System.out.println("Before conversion, int Value"+" = "+No1);
+   System.out.println("After conversion, long Value"+" = "+No2);
+   System.out.println("After conversion, float Value"+" = "+No3);
+  }
+}
+
+ 
