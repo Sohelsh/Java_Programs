ccc6e6735b416b18b1a6632f4d6fa56a84dda5a0
Sohelsh <sohelshaikhadam@gmail.com>
2024-02-05 23:16:19 +0530

    Practice

---
+++ 43.For_Each/program4.java
@@ -0,0 +1,12 @@
+class program4
+{
+  public static void main(String args[])
+  {
+    String[] names = {"Jamal", "Emily", "Destiny", "Mateo"};
+
+    for (String name : names)
+    {
+      System.out.println(name);
+    }
+ }
+}
