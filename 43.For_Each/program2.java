51b4f267230cc81b4014b1633fd469682ad01c9a
Sohelsh <sohelshaikhadam@gmail.com>
2024-02-05 23:15:03 +0530

    Practice

---
+++ 43.For_Each/program2.java
@@ -0,0 +1,12 @@
+public class program2
+{
+  public static void main(String args[])
+  {
+    double arr[] = {14.4,87.4,69.4,48.5,63.5};
+
+    for(double i:arr)
+    {
+      System.out.println(i);
+     }
+  }
+}
