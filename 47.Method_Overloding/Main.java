cfb67b4a989e428557eb49ff01c72b2456fe116a
Sohelsh <sohelshaikhadam@gmail.com>
2024-02-23 23:00:38 +0530

    Practice

---
+++ 47.Method_Overloding/Main.java
@@ -0,0 +1,23 @@
+class overlod
+{
+  void Add(float No1, float No2)
+  {
+    System.out.println("Two Numbers of Addition = "+(No1+No2));
+  }
+  
+   void Add(float No1, float No2,float No3)
+  {
+    System.out.println("Three Numbers of Addition = "+(No1+No2+No3));
+  }
+}
+
+class Main
+{
+  public static void main(String[] args)
+  {
+    overlod Obj = new overlod();
+    Obj.Add(2.3f,7.8f);
+    Obj.Add(4.5f,9.8f,6.6f);
+  }
+}
+    
